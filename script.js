7fd6d9db9d872cfd1c7b07efd6bb008b497263c6
sonu7544 <141615533+sonu7544@users.noreply.github.com>
2024-04-02 23:43:34 +0530

    Dom

---
+++ script.js
@@ -0,0 +1,33 @@
+//getting the html elements
+const decrementBtn = document.getElementById("decrementBtn");
+const incrementBtn = document.getElementById("incrementBtn");
+const resetBtn = document.getElementById("resetBtn");
+const displayvalue = document.getElementById("DisplayValue");
+
+
+decrementBtn.addEventListener("click", ()=> {
+    const value = Number(displayValue.innerText);
+    if(value > 0){
+        displayValue.innerText = value-1;
+    }
+    else{
+        alert("Negative value is not allowed")
+    }
+});
+
+// for increment btn click
+incrementBtn.addEventListener("click", ()=> {
+    const value = Number(displayValue.innerText);
+    if(value >= 10){
+        alert("10+ values are not allowed");
+    }
+    else{
+        displayValue.innerText = value+1;
+    }
+})
+
+
+//for reset btn click
+resetBtn.addEventListener("click", ()=> {
+    displayValue.innerText = 0;
+})
